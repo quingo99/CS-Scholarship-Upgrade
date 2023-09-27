5024bc552d38303bfb55c531b7e08821d2978d17
Qui Ngo <quingo@mail.weber.edu>
2023-09-26 20:45:43 -0600

    Delete everything to redo it

--- ProjectFiles/WCS/WCS/wwwroot/js/wcs.studentprofile.js
+++
@@ -1,6 +0,0 @@
-﻿//Init all the pop overs on the page.
-$(function () {
-    console.log('Init. Tooltips');
-    //$('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-})
